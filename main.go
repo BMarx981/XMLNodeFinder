33e0f6e38bde3b57795c2e3d3ca70a295d2709b9
Brian Marx <brian.marx@expersolve.com>
2019-07-02 15:30:45 -0400

    get code from Github

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ type Root struct {
 //DataInput : contain the meat of the data
 type DataInput struct {
 	XMLName xml.Name `xml:"dataInput"`
-	Program Program  `xml:"program"`
-	Payload Payload  `xml:"payload"`
+	Program Program  `xml:"program>,any"`
+	Payload Payload  `xml:"payload>,any"`
 }
 
 //Program : has the top level data
@@ -30,7 +30,7 @@ type Program struct {
 //Payload : Contains all of the data
 type Payload struct {
 	XMLName xml.Name `xml:"payload"`
-	Values  []Value  `xml:"any"`
+	Values  []Value  `xml:",any"`
 }
 
 //Value : contains the nodes of the payload
@@ -43,8 +43,8 @@ func main() {
 	fmt.Print("Enter the file name: ")
 	reader := bufio.NewReader(os.Stdin)
 
-	fileName, _, err := reader.ReadLine()
-	if err != nil {
+	fileName, _, error := reader.ReadLine()
+	if error != nil {
 		fmt.Println("Error reading file")
 	}
 
@@ -52,23 +52,17 @@ func main() {
 	if err != nil {
 		fmt.Println("Error opening the xml file", err)
 	}
-	processXML(&xmlFile)
-	fmt.Println("End of program")
-} // C:\Clients\YRC\BASELINE2\B.CCS985\BASELINE_JELLYSTONE.xml
+	processXML(xmlFile)
+	fmt.Println("The End********************")
+} // /Users/brianmarx/Desktop/baselineFake.txt
 
 //ProcessXML : the XML nodes.
-func processXML(xmlFile *[]byte) (i int) {
+func processXML(xmlFile []byte) {
 	var root Root
-	xml.Unmarshal(*xmlFile, &root)
-	inputs := root.DataInput
-	for i, inp := range inputs {
-		j := 0
-		fmt.Println(i)
-		for n := 0; n < len(inp.Payload.Values); n++ {
-			fmt.Println(inp.Payload.Values[n].Content)
-		}
-
-		j = j + 1
-	}
-	return 5
+	var prog Program
+	xml.Unmarshal(xmlFile, &prog)
+	xml.Unmarshal(xmlFile, &root)
+	progs := prog.Values
+	// inputs := root.DataInput
+	fmt.Println(len(progs))
 }

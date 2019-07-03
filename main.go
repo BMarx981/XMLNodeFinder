f245c6b7d8d61990fd52bdf78a25111ccef6b1ae
Brian Marx <brian.marx@expersolve.com>
2019-07-03 10:15:34 -0400

    commit before pull

--- main.go
+++ main.go
@@ -16,20 +16,26 @@ type Root struct {
 
 //DataInput : contain the meat of the data
 type DataInput struct {
-	XMLName xml.Name `xml:"dataInput"`
-	Program Program  `xml:"program>,any"`
-	Payload Payload  `xml:"payload>,any"`
+	XMLName xml.Name  `xml:"dataInput"`
+	Hdr     []Hdr     `xml:"hdr"`
+	Payload []Payload `xml:"payload"`
 }
 
-//Program : has the top level data
-type Program struct {
-	XMLName xml.Name `xml:"program"`
+//Hdr : has the top level data
+type Hdr struct {
+	XMLName xml.Name `xml:"hdr"`
 	Values  []Value  `xml:",any"`
 }
 
 //Payload : Contains all of the data
 type Payload struct {
 	XMLName xml.Name `xml:"payload"`
+	Values  []Value  `xml:",any"`
+}
+
+//Record : Contains all of the very important data
+type Record struct {
+	XMLName xml.Name `xml:"record"`
 	Values  []Value  `xml:",any"`
 }
 
@@ -40,6 +46,11 @@ type Value struct {
 }
 
 func main() {
+	processXML(findFile())
+	fmt.Println("The End********************")
+} // /Users/brianmarx/Desktop/baselineFake.txt
+
+func findFile() []byte {
 	fmt.Print("Enter the file name: ")
 	reader := bufio.NewReader(os.Stdin)
 
@@ -52,17 +63,33 @@ func main() {
 	if err != nil {
 		fmt.Println("Error opening the xml file", err)
 	}
-	processXML(xmlFile)
-	fmt.Println("The End********************")
-} // /Users/brianmarx/Desktop/baselineFake.txt
+	return xmlFile
+}
 
 //ProcessXML : the XML nodes.
 func processXML(xmlFile []byte) {
 	var root Root
-	var prog Program
-	xml.Unmarshal(xmlFile, &prog)
-	xml.Unmarshal(xmlFile, &root)
-	progs := prog.Values
-	// inputs := root.DataInput
-	fmt.Println(len(progs))
+
+	error := xml.Unmarshal(xmlFile, &root)
+	if error != nil {
+		fmt.Println(error)
+	}
+	processDataInput(root.DataInput, xmlFile)
+}
+
+func processDataInput(input []DataInput, xmlFile []byte) error {
+	var hdr Hdr
+	xml.Unmarshal(xmlFile, &hdr)
+	for index, element := range input {
+		fmt.Println(index)
+		fmt.Println(element)
+	}
+
+	for index, element := range hdr.Values {
+		fmt.Println("inside hdr loop")
+		fmt.Println(index)
+		fmt.Println(element)
+	}
+
+	return nil
 }
